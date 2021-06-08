e7f16f5eec3f93dccb11196f6c1a9b61cba1c978
TardisSyntax666 <michael@pannier.com.au>
2021-06-09 09:04:42 +1000

    Changed main from running in a class to in a function

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("DiscordClicker")
 pygame.display.set_icon(ICON)
 
+WHITE = (255, 255, 255)
 
-class Main:
 
-    def __init__(self):
-        run = True
-        while run:
+def window_draw():
+    WINDOW.fill(WHITE)
+    pygame.display.update()
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
 
-        pygame.quit()
-        quit()
+def main():
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        window_draw()
+
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
-    main = Main()
+    main()
