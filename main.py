31e0a7b65f44f11435f1d3f43f6de8b8af00d7cf
TardisSyntax666 <michael@pannier.com.au>
2021-06-09 08:19:06 +1000

    Created a window

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+
+WIDTH, HEIGHT = 1000, 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("DiscordClicker")
+
+
+class Main:
+
+    def __init__(self):
+        run = True
+        while run:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+        pygame.quit()
+        quit()
+
+
+if __name__ == "__main__":
+    main = Main()
