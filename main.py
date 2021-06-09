81d4129232cb4f986d8462943cd17a57de522b52
TardisSyntax666 <michael@pannier.com.au>
2021-06-09 12:45:10 +1000

    Created clickerbutton class and made the clicker button clickable which incrememnets a members variable once for every click

--- main.py
+++ main.py
@@ -11,24 +11,50 @@ WHITE = (255, 255, 255)
 
 FPS = 60
 
+CLICKER_IMAGE_SIZE = (200, 200)
 CLICKER_IMAGE = pygame.image.load(os.path.join('assets', 'DiscordClicker_asset_MiniDiscord.png')).convert_alpha()
-CLICKER_IMAGE = pygame.transform.smoothscale(CLICKER_IMAGE, (200, 200))
+CLICKER_IMAGE = pygame.transform.smoothscale(CLICKER_IMAGE, CLICKER_IMAGE_SIZE)
+CLICKER_BUTTON_LOCATION = (400, 200)
+
+members = 0
+
+
+class ClickerButton:
+
+    def __init__(self, size, pos):
+        self.width = size[0]
+        self.height = size[1]
+        self.x = pos[0]
+        self.y = pos[1]
+
+    def is_over(self, pos):
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
+                return True
+
+
+CLICKER_BUTTON = ClickerButton(CLICKER_IMAGE_SIZE, CLICKER_BUTTON_LOCATION)
 
 
 def window_draw():
     WINDOW.fill(WHITE)
-    WINDOW.blit(CLICKER_IMAGE, (400, 200))
+    WINDOW.blit(CLICKER_IMAGE, CLICKER_BUTTON_LOCATION)
     pygame.display.update()
 
 
-def main():
+def main(members):
     clock = pygame.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if CLICKER_BUTTON.is_over(pos):
+                    members += 1
+                    print(members)
 
         window_draw()
 
@@ -37,4 +63,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(members)
