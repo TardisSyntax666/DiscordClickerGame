d4c03b99bcee7f3dd9b03d06226899bf6dc39f21
TardisSyntax666 <michael@pannier.com.au>
2021-06-09 17:20:43 +1000

    Added 5 out of the 10 upgrade buttons

--- button.py
+++ button.py
@@ -10,13 +10,14 @@ class Button:
         self.y = pos[1]
         self.current_x = self.x
         self.current_y = self.y
+        self.shrink_distance = int((self.width - int(self.width*0.9))/2)
         self.surface = button_surface
-        self.pressed_surface = pygame.transform.smoothscale(button_surface, (180, 180))
+        self.pressed_surface = pygame.transform.smoothscale(button_surface, (int(self.width*0.9), int(self.height*0.9)))
         self.current_surface = self.surface
         self.shadow = shadow_surface
         self.not_shadow = not_shadow_surface
-        self.pressed_shadow = pygame.transform.smoothscale(shadow_surface, (190, 190))
-        self.pressed_not_shadow = pygame.transform.smoothscale(not_shadow_surface, (190, 190))
+        self.pressed_shadow = pygame.transform.smoothscale(shadow_surface, (int(shadow_surface.get_rect().size[0]*0.9), int(shadow_surface.get_rect().size[1]*0.9)))
+        self.pressed_not_shadow = pygame.transform.smoothscale(not_shadow_surface, (int(shadow_surface.get_rect().size[0]*0.9), int(shadow_surface.get_rect().size[1]*0.9)))
         self.current_shadow = self.shadow
         self.is_pressed = False
 
--- main.py
+++ main.py
@@ -24,24 +24,43 @@ SHADOW_IMAGE = pygame.transform.smoothscale(SHADOW_IMAGE, (210, 210))
 NOT_SHADOW_IMAGE = pygame.image.load(os.path.join('assets', 'not_shadow.png')).convert_alpha()
 NOT_SHADOW_IMAGE = pygame.transform.smoothscale(NOT_SHADOW_IMAGE, (210, 210))
 
+TIER_IMAGE_SIZE = (100, 100)
+TIER_ONE = pygame.image.load(os.path.join('assets', 'tier_1.png')).convert_alpha()
+TIER_TWO = pygame.image.load(os.path.join('assets', 'tier_2.png')).convert_alpha()
+TIER_THREE = pygame.image.load(os.path.join('assets', 'tier_3.png')).convert_alpha()
+TIER_FOUR = pygame.image.load(os.path.join('assets', 'tier_4.png')).convert_alpha()
+TIER_FIVE = pygame.image.load(os.path.join('assets', 'tier_5.png')).convert_alpha()
+TIER_SHADOW = pygame.image.load(os.path.join('assets', 'tier_shadow.png')).convert_alpha()
+TIER_NOT_SHADOW = pygame.image.load(os.path.join('assets', 'tier_not_shadow.png')).convert_alpha()
+
 BACKGROUND_IMAGE = pygame.image.load(os.path.join('assets', 'background.png')).convert_alpha()
 INFO_BAR_IMAGE = pygame.image.load(os.path.join('assets', 'info_bar.png')).convert_alpha()
 
 members = 0
 buttonlist = []
 
-
 CLICKER_BUTTON = b.Button(CLICKER_IMAGE_SIZE, CLICKER_BUTTON_LOCATION,
-                               CLICKER_IMAGE, SHADOW_IMAGE, NOT_SHADOW_IMAGE)
+                          CLICKER_IMAGE, SHADOW_IMAGE, NOT_SHADOW_IMAGE)
+TIER_ONE_BUTTON = b.Button(TIER_IMAGE_SIZE, (10, 16), TIER_ONE, TIER_SHADOW, TIER_NOT_SHADOW)
+TIER_TWO_BUTTON = b.Button(TIER_IMAGE_SIZE, (10, 131), TIER_TWO, TIER_SHADOW, TIER_NOT_SHADOW)
+TIER_THREE_BUTTON = b.Button(TIER_IMAGE_SIZE, (10, 247), TIER_THREE, TIER_SHADOW, TIER_NOT_SHADOW)
+TIER_FOUR_BUTTON = b.Button(TIER_IMAGE_SIZE, (10, 363), TIER_FOUR, TIER_SHADOW, TIER_NOT_SHADOW)
+TIER_FIVE_BUTTON = b.Button(TIER_IMAGE_SIZE, (10, 479), TIER_FIVE, TIER_SHADOW, TIER_NOT_SHADOW)
+
 buttonlist.append(CLICKER_BUTTON)
+buttonlist.append(TIER_ONE_BUTTON)
+buttonlist.append(TIER_TWO_BUTTON)
+buttonlist.append(TIER_THREE_BUTTON)
+buttonlist.append(TIER_FOUR_BUTTON)
+buttonlist.append(TIER_FIVE_BUTTON)
 
 
 def window_draw(buttonlist, members_text):
     WINDOW.blit(BACKGROUND_IMAGE, (0, 0))
     WINDOW.blit(INFO_BAR_IMAGE, (0, 600))
-    WINDOW.blit(members_text, (500-(members_text.get_rect().size[0])/2, 650))
+    WINDOW.blit(members_text, (500 - (members_text.get_rect().size[0]) / 2, 650))
     label = FONT.render("Server Member Count:", False, WHITE)
-    WINDOW.blit(label, (500-(label.get_rect().size[0])/2, 610))
+    WINDOW.blit(label, (500 - (label.get_rect().size[0]) / 2, 610))
     for button in buttonlist:
         WINDOW.blit(button.current_shadow, (button.current_x - 5, button.current_y - 5))
         WINDOW.blit(button.current_surface, (button.current_x, button.current_y))
@@ -69,8 +88,8 @@ def main(members):
                 if mouse_button_state[0]:
                     button.current_surface = button.pressed_surface
                     button.current_shadow = button.pressed_not_shadow
-                    button.current_x = button.x + 10
-                    button.current_y = button.y + 10
+                    button.current_x = button.x + button.shrink_distance
+                    button.current_y = button.y + button.shrink_distance
                     button.is_pressed = True
                 else:
                     button.current_surface = button.surface
